d886f23daa7b5f59be7822f35f1abbfb95f8b266
Derek <bdai9798@gmail.com>
2018-09-19 20:57:22 +0200

    second revision

--- js/script.js
+++ js/script.js
@@ -2,8 +2,11 @@ console.log('app is alive');
 
 
 function switchChannel(channelName) {
-    console.log('Tuning into channel' + channelName);
+    /* updated channel name is logged to console.*/ 
+    console.log('Tuning into channel' ,channelName);
+    /* channel name above the chat area will be changed when click on channel name list*/ 
     document.getElementById('activechannel').innerHTML = channelName;
+    /* updated channel has a link */ 
     document.getElementById('channelloc').innerHTML = 
     'by <a href="http://w3w.co/upgrading.never.helps" target="_blank"><strong>upgrading.never.helps</strong></a>';
     document.getElementById('onload-star').src='http://ip.lfe.mw.tum.de/sections/star-o.png'
@@ -13,17 +16,20 @@ function switchChannel(channelName) {
 
 }
 function switchstar(){
- 
+    /*click on star in app changes the star from empty to filled*/ 
   $('#onload-star').attr('src','http://ip.lfe.mw.tum.de/sections/star.png');
 
 }
 function selectTab(tabId){
+    /* clicked tab is logged to console*/ 
     console.log('Changing to tab'+tabId);
+    /* #highlighting click on tab remove highlighting from all other tabs*/ 
     $('#tab-bar button').removeClass('selected');
+    /* #highlighting click tab adds highlighting to that ta*/ 
     $('#'+tabId).addClass('selected');
 
 }
 function emojitoggle(){
-
+    /* #emogji using jQuery to selector*/  
     $('#chat-bar #emojis').toggle();
 }
